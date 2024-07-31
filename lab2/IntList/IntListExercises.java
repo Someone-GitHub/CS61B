8625306491e0e15c8d7ab8b3bd7d20a9d11c68cb
Someone <cnxinghaojiang@gmail.com>
2024-07-31 17:11:21 +0800

    1

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -76,7 +76,7 @@ public class IntListExercises {
         if (currElemIsPrime) {
             lst.first *= lst.first;
         }
-        squarePrimes(lst.rest);
-        return true;
+
+        return squarePrimes(lst.rest)||currElemIsPrime ;
     }
 }
