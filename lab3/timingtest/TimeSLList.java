6273f01749e4deb82751221a415ea66c66f1f536
Someone <cnxinghaojiang@gmail.com>
2024-08-04 22:35:10 +0800

    SLList修改完成

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,23 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        Ns.addLast(1000);Ns.addLast(2000);Ns.addLast(4000);Ns.addLast(8000);Ns.addLast(16000);Ns.addLast(32000);Ns.addLast(64000);Ns.addLast(128000);
+        opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);opCounts.addLast(10000);
+
+        for(int a=0;a<Ns.size();a+=1){
+            SLList<Integer> OP = new SLList<>();
+            for(int i = 0;i<Ns.get(a);i+=1){OP.addLast(1);}
+            Stopwatch fir = new Stopwatch();
+            for(int i = 0;i<opCounts.get(a);i+=1){OP.addLast(1);}
+            double time = fir.elapsedTime();
+            times.addLast(time);
+        }
+
+        printTimingTable(Ns,times,opCounts);
     }
 
 }
