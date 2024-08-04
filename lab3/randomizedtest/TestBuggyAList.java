7019336e5453426963e1209848aa4b923f4f0477
Someone <cnxinghaojiang@gmail.com>
2024-08-05 01:45:35 +0800

    SLList修改完成

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,64 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> right = new AListNoResizing<>();
+        BuggyAList<Integer> wrong = new BuggyAList<>();
+
+        right.addLast(4);
+        right.addLast(5);
+        right.addLast(6);
+        wrong.addLast(4);
+        wrong.addLast(5);
+        wrong.addLast(6);
+
+        assertEquals(right.removeLast(), wrong.removeLast());
+        assertEquals(right.removeLast(), wrong.removeLast());
+        assertEquals(right.removeLast(), wrong.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size1 = correct.size();
+                int size2 = broken.size();
+                System.out.println("size in AListNoResizing: " + size1);
+                System.out.println("size in BuggyAList: " + size2);
+            } else if (operationNumber == 2) {
+                // getLast
+                if (correct.size() > 0) {
+                    int lastVal1 = correct.getLast();
+                    System.out.println("last element in AListNoResizing: " + lastVal1);
+                }
+                if (broken.size() > 0) {
+                    int lastVal2 = broken.getLast();
+                    System.out.println("last element in BuggyAList: " + lastVal2);
+                }
+            } else if (operationNumber == 3) {
+                //removeLast
+                if (correct.size() > 0) {
+                    int removeVal1 = correct.removeLast();
+                    System.out.println("removeLast in AListNoResizing: " + removeVal1);
+                }
+                if (broken.size() > 0) {
+                    int removeVal2 = broken.removeLast();
+                    System.out.println("removeLast in BuggyAList: " + removeVal2);
+                }
+            }
+        }
+    }
 }
